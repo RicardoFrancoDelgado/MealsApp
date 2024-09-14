fa20b0a48cef9ba02c49c69cd2e88e8be870d8b5
Ricardo Franco Delgado <rf041555@gmail.com>
2024-09-14 08:54:23 -0300

    usando navigate para as paginas de meals overview

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style={"light"} />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          {/* name = Titulo que vai aparecer na barra superior e component é a tela que vai ser carregada */}
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,6 +1,6 @@
 import { StyleSheet, View, Pressable, Text, Platform } from "react-native";
 
-function CategoryGridTile({ title, color }) {
+function CategoryGridTile({ title, color, onPress }) {
   return (
     <View style={[styles.gridItem]}>
       <Pressable
@@ -9,6 +9,7 @@ function CategoryGridTile({ title, color }) {
           styles.button,
           pressed ? styles.buttonPressed : null,
         ]}
+        onPress={onPress}
       >
         <View style={[styles.itemContainer, , { backgroundColor: color }]}>
           <Text style={styles.title}>{title}</Text>
