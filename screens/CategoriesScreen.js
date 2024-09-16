af013440d6d5ea0af97fb8c21ae6205b826d2167
Ricardo Franco Delgado <rf041555@gmail.com>
2024-09-16 15:44:05 -0300

    carregando itens do meals category

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -5,7 +5,9 @@ import CategoryGridTile from "../components/CategoryGridTile";
 function CategoriesScreen({ navigation }) {
   function renderCategoryItem(itemData) {
     function onPressHandler() {
-      navigation.navigate("MealsOverview");
+      navigation.navigate("MealsOverview", {
+        categoryId: itemData.item.id,
+      });
     }
  
     return (
